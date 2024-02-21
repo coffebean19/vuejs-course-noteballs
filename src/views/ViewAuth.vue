580e5f87e1e735d8b604b8e317acbb5416e3fffa
coffebean19 <istvan.mayer1999@protonmail.com>
2024-02-21 10:59:36 +0200

    Section 21 start.

---
+++ src/views/ViewAuth.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="auth">
+    <div class="tabs is-centered">
+      <ul>
+        <li :class="{ 'is-active': !register }" @click.prevent="register = false">
+          <a href="">Login</a>
+        </li>
+        <li :class="{ 'is-active': register }" @click.prevent=" register = true"">
+              <a href="">Register</a>
+            </li>
+
+          </ul>
+
+
+        </div>
+      </div>
+</template>
+
+<script setup>
+// imports
+import { ref } from 'vue';
+
+// register / login
+
+const register = ref(false);
+
+</script>
